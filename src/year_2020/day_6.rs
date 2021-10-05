2e72a1259920419333100f9b0b4aeafeee1949f0
John Doe <paladinarr+gitlab@gmail.com>
2021-10-05 00:20:17 +0000

    fmt

--- src/year_2020/day_6.rs
+++ src/year_2020/day_6.rs
@@ -1,6 +1,6 @@
 const QUESTIONS: [char; 26] = [
-	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-	's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+	't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
 pub fn part_a(input: String) -> String {
@@ -9,7 +9,10 @@ pub fn part_a(input: String) -> String {
 }
 
 fn count_any_yes(group: &str) -> u32 {
-	QUESTIONS.iter().filter(|&&char| group.contains(char)).count() as u32
+	QUESTIONS
+		.iter()
+		.filter(|&&char| group.contains(char))
+		.count() as u32
 }
 
 pub fn part_b(input: String) -> String {
@@ -18,7 +21,8 @@ pub fn part_b(input: String) -> String {
 }
 
 fn count_all_yes(group: &str) -> u32 {
-	QUESTIONS.iter().filter(|&&char| {
-		group.lines().all(|line| line.contains(char))
-	}).count() as u32
+	QUESTIONS
+		.iter()
+		.filter(|&&char| group.lines().all(|line| line.contains(char)))
+		.count() as u32
 }
