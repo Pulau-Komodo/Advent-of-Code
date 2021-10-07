d8e376760021a4a4a6ac8506fba497435182d44b
John Doe <paladinarr+gitlab@gmail.com>
2021-10-07 11:16:18 +0000

    2020 day 10

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ fn main() {
 		("2020", "8", "a") => year_2020::day_8::part_a(input),
 		("2020", "8", "b") => year_2020::day_8::part_b(input),
 		("2020", "9", _) => year_2020::day_9::get_answers(input),
+		("2020", "10", _) => year_2020::day_10::get_answers(input),
 		_ => unimplemented!(),
 	};
 	println!("{}", result);
---
+++ src/year_2020/day_10.rs
@@ -0,0 +1,37 @@
+pub fn get_answers(input: String) -> String {
+	let mut adapters = input
+		.lines()
+		.map(str::parse::<u16>)
+		.collect::<Result<Vec<_>, _>>()
+		.unwrap();
+	adapters.sort_unstable();
+	let mut previous_number = 0;
+	let mut one_jolt_count = 0;
+	let mut three_jolt_count = 1;
+	let mut one_jolt_cluster_size = 0;
+	let mut possibilities: u64 = 1;
+	for number in adapters {
+		let difference = number - previous_number;
+		previous_number = number;
+		if difference == 1 {
+			one_jolt_count += 1;
+			one_jolt_cluster_size += 1;
+		} else if difference == 3 {
+			three_jolt_count += 1;
+			possibilities *= calculate_possibilities(one_jolt_cluster_size);
+			one_jolt_cluster_size = 0;
+		}
+	}
+	let product = one_jolt_count * three_jolt_count;
+	format!("1: {}, 2: {}", product, possibilities)
+}
+
+pub fn calculate_possibilities(one_jolt_cluster_size: u16) -> u64 {
+	match one_jolt_cluster_size {
+		0..=1 => 1,
+		2 => 2,
+		3 => 4,
+		4 => 7,
+		_ => unreachable!(),
+	}
+}
