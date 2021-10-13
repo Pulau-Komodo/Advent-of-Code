31375fb7e4dc95590cb3ac6a0c2eab3d9be1a7c7
John Doe <paladinarr+gitlab@gmail.com>
2021-10-13 10:39:44 +0000

    More sensible math Now more legible, and handles first bus not having index 0

--- src/year_2020/day_13.rs
+++ src/year_2020/day_13.rs
@@ -17,19 +17,22 @@ fn parse_input(input: String) -> (i32, Vec<(usize, i32)>) {
 	(time, buses)
 }
 
-fn find_first_cool_timestamp(buses: &Vec<(usize, i32)>) -> u64 {
-	let mut buses_iterator = buses.iter();
-	let (first_timeslot, first_interval) = buses_iterator.next().unwrap();
-	let mut starting_point = *first_timeslot as u64;
-	let mut cycle = *first_interval as u64;
-	for (target, interval) in buses_iterator {
-		let target = *target as u64;
+fn find_first_cool_timestamp(buses: &[(usize, i32)]) -> u64 {
+	let mut cycle = 1;
+	let mut starting_point = 0;
+	for (reverse_target, interval) in buses {
 		let interval = *interval as u64;
-		let cycles = how_many_cycles_to_target(starting_point % interval, target % interval, cycle % interval, interval);
+		let target = interval - *reverse_target as u64 % interval;
+		let cycles = how_many_cycles_to_target(
+			starting_point % interval,
+			target % interval,
+			cycle % interval,
+			interval,
+		);
 		starting_point += cycles * cycle;
 		cycle *= interval;
 	}
-	cycle - starting_point
+	starting_point
 }
 
 fn how_many_cycles_to_target(start: u64, target: u64, step: u64, modulo: u64) -> u64 {
@@ -37,7 +40,7 @@ fn how_many_cycles_to_target(start: u64, target: u64, step: u64, modulo: u64) ->
 	let mut current = start;
 	loop {
 		if current == target {
-			break cycles
+			break cycles;
 		}
 		cycles += 1;
 		current = (current + step) % modulo;
@@ -60,9 +63,24 @@ pub fn get_answers(input: String) -> String {
 mod tests {
 	use super::*;
 	#[test]
-	fn bag_rule() {
+	fn sample_input() {
 		let input = String::from("939\n7,13,x,x,59,x,31,19");
 		assert_eq!(get_answers(input), String::from("1: 295, 2: 1068781"));
+	}
+	#[test]
+	fn non_zero_first() {
+		let input =
+			String::from("0\nx,x,x,x,x,x,x,x,x,x,31,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,7");
+		assert_eq!(get_answers(input), String::from("1: 49, 2: 145"));
+	}
+	#[test]
+	fn full_input() {
+		let input =
+			std::fs::read_to_string("./input/2020/13.txt").expect("Could not read file");
+		assert_eq!(
+			get_answers(input),
+			String::from("1: 2298, 2: 783685719679632")
+		);
 	}
 	#[test]
 	fn cycle_counter() {
