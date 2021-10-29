f53f3693911d22c7b955735ab7b03ba5bff3ee63
John Doe <paladinarr+gitlab@gmail.com>
2021-10-29 11:56:29 +0000

    fmt

--- year_2015/src/bin/day_1.rs
+++ year_2015/src/bin/day_1.rs
@@ -5,11 +5,15 @@ fn main() {
 fn get_answers(input: &str) -> String {
 	let mut floor = 0;
 	let mut first_basement = 0;
-	for (index, change) in input.chars().map(|char| match char {
-		'(' => 1,
-		')' => -1,
-		_ => panic!(),
-	}).enumerate() {
+	for (index, change) in input
+		.chars()
+		.map(|char| match char {
+			'(' => 1,
+			')' => -1,
+			_ => panic!(),
+		})
+		.enumerate()
+	{
 		floor += change;
 		if first_basement == 0 && floor == -1 {
 			first_basement = index + 1;
