f559f101b68048207da45f19b118a6b174961488
John Doe <paladinarr+gitlab@gmail.com>
2021-10-30 22:30:38 +0000

    2015 day 8

---
+++ year_2015/src/bin/day_8.rs
@@ -0,0 +1,41 @@
+fn main() {
+	shared::print_answers(8, &[get_answer_1, get_answer_2]);
+}
+
+fn get_answer_1(input: &str) -> u32 {
+	input
+		.lines()
+		.map(|line| {
+			let mut coding_char_count = 2;
+			let mut skip: u8 = 0;
+			for (index, char) in line.char_indices() {
+				if index == 0 || index == line.len() - 1 {
+					continue;
+				}
+				if skip == 1 && char == 'x' {
+					skip = 2;
+					coding_char_count += 2;
+				} else if skip > 0 {
+					skip -= 1;
+					continue;
+				}
+				if char == '\\' {
+					skip = 1;
+					coding_char_count += 1;
+				}
+			}
+			coding_char_count
+		})
+		.sum()
+}
+
+fn get_answer_2(input: &str) -> u32 {
+	input
+		.lines()
+		.map(|line| {
+			line.chars()
+				.filter(|&char| char == '"' || char == '\\')
+				.count() as u32 + 2
+		})
+		.sum()
+}
