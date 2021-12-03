716482c6df706eb450b908a7647874a452ca3188
John Doe <paladinarr+gitlab@gmail.com>
2021-12-03 20:31:23 +0000

    Use bitwise operations

--- year_2021/src/bin/day_3.rs
+++ year_2021/src/bin/day_3.rs
@@ -12,86 +12,70 @@ fn get_answer_2(input: &str) -> u32 {
 	get_life_support_rating(&report)
 }
 
-fn parse_input(input: &str) -> Vec<[bool; 12]> {
-	input.lines().map(parse_line).collect()
+fn parse_input(input: &str) -> Vec<u16> {
+	input
+		.lines()
+		.map(|line| u16::from_str_radix(line, 2).unwrap())
+		.collect()
 }
 
-fn parse_line(input: &str) -> [bool; 12] {
-	let mut output = [false; 12];
-	for (index, char) in input.char_indices() {
-		if char == '1' {
-			output[index] = true;
+fn gamma_rate(report: &[u16]) -> u16 {
+	let mut output = 0;
+	for i in 0..12 {
+		if most_common_in_pos(report, i) {
+			output += 1 << i;
 		}
 	}
 	output
 }
 
-fn most_common(report: &[[bool; 12]]) -> [bool; 12] {
-	let half_length = report.len() / 2;
-	let true_counts = report.iter().fold([0_usize; 12], |mut acc, reading| {
-		for (index, bit) in reading.iter().enumerate() {
-			if *bit {
-				acc[index] += 1;
-			}
-		}
-		acc
-	});
-	let mut output = [false; 12];
-	for (index, count) in true_counts.iter().enumerate() {
-		output[index] = *count >= half_length;
-	}
-	output
+fn get_power_consumption(report: &[u16]) -> u32 {
+	let gamma_rate = gamma_rate(report);
+	let epsilon_rate = !gamma_rate & 0b0000_1111_1111_1111;
+	gamma_rate as u32 * epsilon_rate as u32
 }
 
-fn get_power_consumption(report: &[[bool; 12]]) -> u32 {
-	let most_common = most_common(report);
-	let mut gamma_rate = 0;
-	let mut epsilon_rate = 0;
-	for (index, &on) in most_common.iter().rev().enumerate() {
-		if on {
-			gamma_rate += 2_u32.pow(index as u32);
-		} else {
-			epsilon_rate += 2_u32.pow(index as u32);
-		}
-	}
-	gamma_rate * epsilon_rate
+fn get_life_support_rating(report: &[u16]) -> u32 {
+	let oxygen_generator_rating = find_rating(report, true);
+	let co2_scrubber_rating = find_rating(report, false);
+	oxygen_generator_rating as u32 * co2_scrubber_rating as u32
 }
 
-fn get_life_support_rating(report: &[[bool; 12]]) -> u32 {
-	let oxygen_generator_entry = find_entry(report, true);
-	let co2_scrubber_entry = find_entry(report, false);
-	make_number(&oxygen_generator_entry) * make_number(&co2_scrubber_entry)
-}
-
-fn find_entry(report: &[[bool; 12]], most: bool) -> [bool; 12] {
+fn find_rating(report: &[u16], most: bool) -> u16 {
 	let mut filtered_report = report.to_vec();
-	for index in 0..12 {
-		let most_common = most_common_in_pos(&filtered_report, index);
+	for i in (0..12).rev() {
+		let most_common = most_common_in_pos(&filtered_report, i);
+		let target = most == most_common;
 		filtered_report = filtered_report
 			.into_iter()
-			.filter(|reading| (reading[index] == most_common) == most)
+			.filter(|reading| (1 << i & reading != 0) == target)
 			.collect();
 		if filtered_report.len() == 1 {
 			return *filtered_report.get(0).unwrap();
 		}
 	}
-	panic!();
+	panic!("Did not found a unique answer");
 }
 
-fn most_common_in_pos(report: &[[bool; 12]], pos: usize) -> bool {
-	report.iter().filter(|reading| reading[pos]).count() * 2 >= report.len()
-}
-
-fn make_number(reading: &[bool; 12]) -> u32 {
-	reading
-		.iter()
-		.rev()
-		.enumerate()
-		.fold(0, |acc, (index, &on)| {
-			if on {
-				acc + 2_u32.pow(index as u32)
-			} else {
-				acc
-			}
-		})
+fn most_common_in_pos(report: &[u16], pos: u16) -> bool {
+	let half_len = if report.len() % 2 == 0 {
+		report.len() / 2
+	} else {
+		(report.len() + 1) / 2
+	};
+	let mut zeroes = 0;
+	let mut ones = 0;
+	for reading in report {
+		if 1 << pos & reading != 0 {
+			ones += 1;
+		} else {
+			zeroes += 1;
+		}
+		if ones >= half_len {
+			return true;
+		} else if zeroes > half_len {
+			return false;
+		}
+	}
+	ones >= half_len
 }
