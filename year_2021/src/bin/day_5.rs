b09daf5550a25d4835f9f8b895559e8c1e639d48
John Doe <paladinarr+gitlab@gmail.com>
2021-12-05 19:56:38 +0000

    2021 day 5

---
+++ year_2021/src/bin/day_5.rs
@@ -0,0 +1,70 @@
+fn main() {
+	shared::print_answers(5, &[get_answer_1, get_answer_2]);
+}
+
+fn get_answer_1(input: &str) -> u32 {
+	let include_diagonals = false;
+	let vents = vent_map(parse_input(input, include_diagonals));
+	vents.iter().filter(|(_, &count)| count > 1).count() as u32
+}
+
+fn get_answer_2(input: &str) -> u32 {
+	let include_diagonals = true;
+	let vents = vent_map(parse_input(input, include_diagonals));
+	vents.iter().filter(|(_, &count)| count > 1).count() as u32
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct Point {
+	x: i16,
+	y: i16,
+}
+
+type Line = (Point, Point);
+
+fn parse_input(
+	input: &str,
+	include_diagonals: bool,
+) -> impl Iterator<Item = Line> + '_ {
+	input
+		.lines()
+		.map(|line| {
+			let (start, end) = line.split_once(" -> ").unwrap();
+			let (start_x, start_y) = start.split_once(',').unwrap();
+			let (end_x, end_y) = end.split_once(',').unwrap();
+			((start_x, start_y), (end_x, end_y))
+		})
+		.filter(move |((start_x, start_y), (end_x, end_y))| {
+			include_diagonals || start_x == end_x || start_y == end_y
+		})
+		.map(|((start_x, start_y), (end_x, end_y))| {
+			(
+				Point {
+					x: start_x.parse().unwrap(),
+					y: start_y.parse().unwrap(),
+				},
+				Point {
+					x: end_x.parse().unwrap(),
+					y: end_y.parse().unwrap()
+				},
+			)
+		})
+}
+
+fn vent_map(lines: impl Iterator<Item = Line>) -> std::collections::HashMap::<Point, u8> {
+	let mut map = std::collections::HashMap::new();
+	for (start, end) in lines {
+		let (d_x, d_y) = (end.x - start.x, end.y - start.y);
+		let steps = d_x.abs().max(d_y.abs());
+		let (s_x, s_y) = (d_x.signum(), d_y.signum());
+		for i in 0..=steps {
+			let point = Point {
+				x: start.x + i * s_x,
+				y: start.y + i * s_y,
+			};
+			let entry = map.entry(point).or_default();
+			*entry += 1;
+		}
+	}
+	map
+}
