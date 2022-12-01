fe95f360269dca9ebfa9ee8d93a5030f2e64da54
John Doe <paladinarr+gitlab@gmail.com>
2022-12-01 20:42:28 +0000

    Make the top 3-finding slightly nicer

--- year_2022/src/bin/day_1.rs
+++ year_2022/src/bin/day_1.rs
@@ -25,17 +25,11 @@ fn get_answer_2(input: &str) -> u32 {
 				.sum::<u32>()
 		})
 		.fold([0, 0, 0], |mut acc, new_value| {
-			let (index, lowest) = acc
-				.iter()
-				.enumerate()
-				.min_by_key(|(_, value)| **value)
-				.unwrap();
-			{
-				if new_value > *lowest {
-					acc[index] = new_value;
-				}
-				acc
+			if new_value > acc[2] {
+				acc[2] = new_value;
+				acc.sort_unstable_by(|a, b| b.cmp(a));
 			}
+			acc
 		})
 		.iter()
 		.sum()
