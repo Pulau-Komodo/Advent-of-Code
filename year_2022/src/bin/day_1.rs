69c51496240fe6b075ac35078effe5233df454c0
John Doe <paladinarr+gitlab@gmail.com>
2022-12-01 20:47:51 +0000

    Make the sort a tiny bit nicer again

--- year_2022/src/bin/day_1.rs
+++ year_2022/src/bin/day_1.rs
@@ -25,9 +25,9 @@ fn get_answer_2(input: &str) -> u32 {
 				.sum::<u32>()
 		})
 		.fold([0, 0, 0], |mut acc, new_value| {
-			if new_value > acc[2] {
-				acc[2] = new_value;
-				acc.sort_unstable_by(|a, b| b.cmp(a));
+			if new_value > acc[0] {
+				acc[0] = new_value;
+				acc.sort_unstable();
 			}
 			acc
 		})
