4f10e74f07821793acb2a7d9c8585293813058cc
Pulau_Komodo <komodo_helico@example.com>
2024-09-09 15:03:34 +0000

    Replace `IntoIterator` trait impls for each int `Range<T>` type with a blanket one

--- shared/src/range.rs
+++ shared/src/range.rs
@@ -36,97 +36,12 @@ impl<T> From<std::ops::Range<T>> for Range<T> {
 	}
 }
 
-impl IntoIterator for Range<u8> {
-	type Item = u8;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<u16> {
-	type Item = u16;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<u32> {
-	type Item = u32;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<u64> {
-	type Item = u64;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<u128> {
-	type Item = u128;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<i8> {
-	type Item = i8;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<i16> {
-	type Item = i16;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<i32> {
-	type Item = i32;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<i64> {
-	type Item = i64;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<i128> {
-	type Item = i128;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<usize> {
-	type Item = usize;
-	type IntoIter = std::ops::Range<Self::Item>;
-
-	fn into_iter(self) -> Self::IntoIter {
-		self.start..self.end
-	}
-}
-impl IntoIterator for Range<isize> {
-	type Item = isize;
-	type IntoIter = std::ops::Range<Self::Item>;
+impl<T> IntoIterator for Range<T>
+where
+	std::ops::Range<T>: Iterator<Item = T>,
+{
+	type Item = T;
+	type IntoIter = std::ops::Range<T>;
 
 	fn into_iter(self) -> Self::IntoIter {
 		self.start..self.end
