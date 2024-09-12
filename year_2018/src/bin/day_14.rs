586407ca1e6f8463cd427b7c2e1851631dcf4914
Pulau_Komodo <komodo_helico@example.com>
2024-09-12 00:49:25 +0000

    2018 day 14

---
+++ year_2018/src/bin/day_14.rs
@@ -0,0 +1,63 @@
+use std::array;
+
+fn main() {
+	shared::print_answers(14, &[get_answer_1, get_answer_2]);
+}
+
+fn get_answer_1(input: &str) -> u64 {
+	let offset = process_input(input);
+	let target = offset + 10;
+
+	let recipes = develop_recipes(|length, _| length == target);
+
+	recipes
+		.into_iter()
+		.skip(offset)
+		.fold(0, |acc, n| acc * 10 + n as u64)
+}
+
+fn get_answer_2(input: &str) -> u64 {
+	let target: Vec<_> = input.trim().bytes().map(|byte| byte - b'0').collect();
+
+	let recipes = develop_recipes(|_, recipes| matches_last_10(recipes, &target));
+
+	recipes.len() as u64 - 10
+}
+
+fn process_input(input: &str) -> usize {
+	input.trim().parse().unwrap()
+}
+
+fn develop_recipes(test: impl Fn(usize, &[u8]) -> bool) -> Vec<u8> {
+	let mut recipes = vec![3, 7];
+	let mut positions: [usize; 2] = array::from_fn(std::convert::identity);
+	loop {
+		let sum: u8 = positions.iter().map(|pos| recipes[*pos]).sum();
+		let digit_one = sum / 10;
+		let digit_two = sum % 10;
+		if digit_one > 0 {
+			recipes.push(digit_one);
+			if test(recipes.len(), &recipes) {
+				break;
+			}
+		}
+		recipes.push(digit_two);
+		if test(recipes.len(), &recipes) {
+			break;
+		}
+		for pos in &mut positions {
+			*pos += 1 + recipes[*pos] as usize;
+			*pos %= recipes.len();
+		}
+	}
+	recipes
+}
+
+fn matches_last_10(recipes: &[u8], target: &[u8]) -> bool {
+	recipes.len() >= 10
+		&& recipes
+			.iter()
+			.skip(recipes.len() - 10)
+			.zip(target)
+			.all(|(a, b)| a == b)
+}
