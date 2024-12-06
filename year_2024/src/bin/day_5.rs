b299b2bacf5d71b8cb0102db00e75be3a683ed71
John Doe <paladinarr+gitlab@gmail.com>
2024-12-06 19:55:46 +0000

    Optimized using `HashSet`

--- year_2024/src/bin/day_5.rs
+++ year_2024/src/bin/day_5.rs
@@ -1,27 +1,29 @@
+use std::collections::HashSet;
+
 fn main() {
 	shared::print_answers(5, &[get_answer_1, get_answer_2]);
 }
 
 fn get_answer_1(input: &str) -> u32 {
 	let (rules, updates) = input.split_once("\n\n").unwrap();
-	let rules = rules.lines().map(Rule::from_line).collect::<Vec<_>>();
+	let rules = rules.lines().map(Rule::from_line).collect::<HashSet<_>>();
 
 	updates
 		.lines()
 		.map(Update::from_line)
-		.filter(|update| rules.iter().all(|rule| rule.test(update)))
+		.filter(|update| update.test(&rules))
 		.map(|update| update.middle() as u32)
 		.sum()
 }
 
 fn get_answer_2(input: &str) -> u32 {
 	let (rules, updates) = input.split_once("\n\n").unwrap();
-	let rules = rules.lines().map(Rule::from_line).collect::<Vec<_>>();
+	let rules = rules.lines().map(Rule::from_line).collect::<HashSet<_>>();
 
 	updates
 		.lines()
 		.map(Update::from_line)
-		.filter(|update| !rules.iter().all(|rule| rule.test(update)))
+		.filter(|update| !update.test(&rules))
 		.map(|mut update| {
 			update.repair(&rules);
 			update
@@ -30,6 +32,7 @@ fn get_answer_2(input: &str) -> u32 {
 		.sum()
 }
 
+#[derive(Debug, Hash, PartialEq, Eq)]
 struct Rule {
 	first: u8,
 	last: u8,
@@ -43,17 +46,6 @@ impl Rule {
 			last: last.parse().unwrap(),
 		}
 	}
-	fn test(&self, update: &Update) -> bool {
-		let mut last_found = false;
-		for &page in &update.pages {
-			if page == self.last {
-				last_found = true;
-			} else if page == self.first {
-				return !last_found;
-			}
-		}
-		true
-	}
 }
 
 struct Update {
@@ -66,20 +58,30 @@ impl Update {
 			pages: line.split(',').map(|n| n.parse().unwrap()).collect(),
 		}
 	}
+	fn test(&self, rules: &HashSet<Rule>) -> bool {
+		for first in 0..self.pages.len() - 1 {
+			for second in first + 1..self.pages.len() {
+				if rules.contains(&Rule {
+					first: self.pages[second],
+					last: self.pages[first],
+				}) {
+					return false;
+				}
+			}
+		}
+		true
+	}
 	fn middle(&self) -> u8 {
 		self.pages[self.pages.len() / 2]
 	}
-	fn repair(&mut self, rules: &[Rule]) {
+	fn repair(&mut self, rules: &HashSet<Rule>) {
 		self.pages.sort_unstable_by(|&a, &b| {
-			let Some(rule) = rules.iter().find(|rule| {
-				rule.first == a && rule.last == b || rule.first == b && rule.last == a
-			}) else {
-				return std::cmp::Ordering::Equal;
-			};
-			if rule.first == a {
+			if rules.contains(&Rule { first: a, last: b }) {
 				std::cmp::Ordering::Less
-			} else {
+			} else if rules.contains(&Rule { first: b, last: a }) {
 				std::cmp::Ordering::Greater
+			} else {
+				std::cmp::Ordering::Equal
 			}
 		});
 	}
